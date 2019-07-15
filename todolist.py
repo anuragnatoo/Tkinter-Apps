517e8087e14fdc84ea563ed3a8a580d7f87b687f
anuragnatoo <coe17b015@iiitdm.ac.in>
2019-07-15 18:38:05 +0530

    Commit-#1--To-Do-List

---
+++ todolist.py
@@ -0,0 +1,13 @@
+from tkinter import *
+window = Tk()
+window.title("To Do List")
+content = Listbox(window,font="Ariel 24 bold",fg="White",bg="Black")
+task = StringVar()
+e = Entry(window, textvariable=task,font="Ariel 24")
+add = Button(window,text="ADD",font="Ariel 20",padx=30,fg="White",bg="Blue",command= lambda content=content,task=task: content.insert(END,task.get()))
+delete = Button(window,text="DELETE",font="Ariel 20",fg="White",bg="Red",command= lambda content=content : content.delete(ANCHOR))
+content.grid(row=0,column=0,columnspan=2,padx=5,pady=10)
+e.grid(row=1,column=0,columnspan=2,padx=5,pady=10)
+add.grid(row=2,column=0,padx=5,pady=20)
+delete.grid(row=2,column=1,padx=5,pady=20)
+window.mainloop()
